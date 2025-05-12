442c154ffe766aa8a039ad9436a194d3302d8f67
tskopen <80185495+tskopen@users.noreply.github.com>
2025-05-12 18:09:48 -0400

    Update main.ino

--- src/main.ino
+++ src/main.ino
@@ -4,4 +4,23 @@
  * Name:    Thor Kopenkoskey
  * Date:    May 12, 2025
  */
+
 #include "OutIMU.ino"
+
+int main();
+{
+  Bool flightControl = false;
+  
+  OutIMU.setup(void)
+  if(!flightControl)
+  {
+    //Checks for launch
+  }
+  
+  While(flightControl)
+  {
+    OutIMU.loop()
+  }
+  
+
+}
