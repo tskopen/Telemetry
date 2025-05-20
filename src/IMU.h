95ea93512d380d74401cb4cc5c7e6f9383a1d3df
tskopen <80185495+tskopen@users.noreply.github.com>
2025-05-19 21:40:56 -0400

    Update IMU.h

--- src/IMU.h
+++ src/IMU.h
@@ -141,10 +141,8 @@
 class IMUClass 
 {
   public:
-  {
     uint8_t readRegister(int file, uint8_t reg);
     int16_t accelData (int file, uint8_t regL, uint8_t regH);
     void setIMU();
     void readIMUData(int ledOUT);
-  }
-}
+};
