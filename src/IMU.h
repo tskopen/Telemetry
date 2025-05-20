44d28e718f526cc0c9f63f85b999eab29b766d2d
tskopen <80185495+tskopen@users.noreply.github.com>
2025-05-19 21:38:35 -0400

    Update IMU.h

--- src/IMU.h
+++ src/IMU.h
@@ -138,8 +138,13 @@
 #define LSM6DSOX_REG_FIFO_DATA_OUT_Z_H   0x7E
 
 // Proto types
-uint8_t readRegister(int file, uint8_t reg);
-int16_t accelData (int file, uint8_t regL, uint8_t regH);
-void setIMU();
-void readIMUData(int ledOUT);
-
+class IMUClass 
+{
+  public:
+  {
+    uint8_t readRegister(int file, uint8_t reg);
+    int16_t accelData (int file, uint8_t regL, uint8_t regH);
+    void setIMU();
+    void readIMUData(int ledOUT);
+  }
+}
