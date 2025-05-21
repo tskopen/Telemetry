68e16418731496d6dbda79bc977f13602cf84032
tskopen <80185495+tskopen@users.noreply.github.com>
2025-05-21 19:20:32 -0400

    Create routines.cpp

---
+++ src/routines.cpp
@@ -0,0 +1,41 @@
+/*
+ * Generalized routines/functions for flight computer... LED BLINK, timers, etc
+ *
+ * Name:    Thor Kopenkoskey
+ * Date:    May 21, 2025
+ */
+#include "routines.h"
+#include <chrono>
+
+using namespace std;
+
+void LEDblinker(unsigned long duration, int pin, int blinks) {
+    wiringPiSetup();
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+
+    static unsigned long startMillis= 0;
+    static int blinkCount = 0;
+
+    // Calculate the interval between each blink
+    unsigned long interval = duration / (blinks);
+  
+    for(int i = 0; i < blinks; i++)
+      // Initialize the timer on the first call
+      if (i < blinks) 
+      {
+          startMillis = millis();
+      }
+  
+      // Check if it's time to toggle the LED state
+      if (millis() - startMillis >= interval) 
+      {
+        digitalWrite(pin, HIGH);
+          
+        startMillis = millis(); // Reset the timer
+        blinkCount++;
+
+        }
+    }
+}
+
