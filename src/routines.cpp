1884fa86e0e0a45e2aecf63c5a2e94c37a66d2bd
tskopen <80185495+tskopen@users.noreply.github.com>
2025-05-22 10:09:54 -0400

    Update routines.cpp

--- src/routines.cpp
+++ src/routines.cpp
@@ -21,22 +21,23 @@ void routineClass::LEDblinker(unsigned long duration, int pin, int blinks)
 
     // Calculate the interval between each blink
     unsigned long interval = duration / (blinks);
-  
-    for(int i = 0; i < blinks; i++)
+    while(blinkCount < blinks)
     {
-      // Initialize the timer on the first call
-      if (i < blinks) 
-      {
-          startMillis = millis();
-      }
-  
-      // Check if it's time to toggle the LED state
-      if (millis() - startMillis >= interval) 
-      {
-        digitalWrite(pin, HIGH);
-        startMillis = millis(); // Reset the timer
-        blinkCount++;
-
+        for(int i = 0; i < blinks; i++)
+        {
+          // Initialize the timer on the first call
+          if (i < blinks) 
+          {
+              startMillis = millis();
+          }
+      
+          // Check if it's time to toggle the LED state
+          if (millis() - startMillis >= interval) 
+          {
+            digitalWrite(pin, HIGH);
+            startMillis = millis(); // Reset the timer
+            blinkCount++;
+          }
       }
     }
 }
