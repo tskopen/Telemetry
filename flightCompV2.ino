6412698d60f5666bddf3d91616d7726690972f8f
tskopen <tskopen@gmail.com>
2025-10-26 21:44:50 -0400

    Minor updates and tweaks
    beginning gui development

--- flightCompV2.ino
+++ flightCompV2.ino
@@ -1,9 +1,9 @@
 #include <sensorfusion.h>
 #include <servos.h>
+#include "flash.h"
 
 
 void setup() {
-  //155200 and above Baud avoid packetloss
   Serial.begin(115200);
 
   Wire.begin(I2C2_SDA, I2C2_SCL);
@@ -16,6 +16,8 @@ void setup() {
   for (int i = 0; i < NUM_SERVOS; i++) {
     servos[i].init_servo();
   }
+
+  setupFlash();
 
 }
 
@@ -44,28 +46,27 @@ void loop(){
   duringFlightOrientationTracking();
   }
   
-  /*
-
+/*
   Serial.print(gForceAccelerationXAxis); Serial.print('\t');
   Serial.print(gForceAccelerationYAxis); Serial.print('\t');
   Serial.print(gForceAccelerationZAxis); Serial.print('\t');
-
+*/
   Serial.print(deadReckoningXAxis); Serial.print('\t');
   Serial.print(deadReckoningYAxis); Serial.print('\t');
   Serial.print(deadReckoningZAxis); Serial.println('\t');
-  */
-
+  
+/*
   Serial.print(gyroAccelerationXAxis); Serial.print('\t');
   Serial.print(gyroAccelerationYAxis); Serial.print('\t');
   Serial.print(gyroAccelerationZAxis); Serial.print('\t');
-
+*/
 
   Serial.print(gyroPositionXAxis); Serial.print('\t');
   Serial.print(gyroPositionYAxis); Serial.print('\t');
   Serial.print(gyroPositionZAxis); Serial.println('\t');
-/*
+
   Serial.print(magneticXAxis); Serial.print('\t');
   Serial.print(magneticYAxis); Serial.print('\t');
   Serial.println(magneticZAxis); 
-  */
+
 }
